6892dba3b900343fd4fe33b4ee5d5bed8dbd4411
Pratham271 <pratham19csu221@ncuindia.edu>
2024-07-17 17:16:23 +0530

    added the copy button

--- src/components/chat/LLMResponseComponent.tsx
+++ src/components/chat/LLMResponseComponent.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import Markdown from 'react-markdown'
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '../ui/card'
 import LLmSkeleton from '../LLmSkeleton'
+import { CopyIcon } from '../ui/Icons';
 
 
 
 const LlmResponseComponent = ({content}:{content:string}) => {
     const hasLLMResponse = content && content.trim().length > 0 
     const cleanedMarkdown = content.replace(/=====/, ''); 
+    const [copyStatus, setCopyStatus] = useState<boolean[]>([]);
+
+  const handleCopy = (index:number) => {
+    setCopyStatus((prev) => {
+      const newCopyStatus = [...prev];
+      newCopyStatus[index] = true;
+      return newCopyStatus;
+    });
+
+    setTimeout(() => {
+      setCopyStatus((prev) => {
+        const newCopyStatus = [...prev];
+        newCopyStatus[index] = false;
+        return newCopyStatus;
+      });
+    }, 2000);
+
+    const code = document.getElementById(`pretag-${index}`)?.innerText;
+    navigator.clipboard.writeText(code!);
+  };
+  let preIndex = 0;
   return (
     <>
        {hasLLMResponse ? (
@@ -22,11 +45,17 @@ const LlmResponseComponent = ({content}:{content:string}) => {
                         code: ({ node, ...props }) => (
                             <code  {...props} className="bg-[#eeeaea] rounded p-[1.5px]  dark:bg-gray-800" />
                           ),
-                          pre: ({ node, ...props }) => (
-                            <div className='relative'>
-                                <pre id='pretag' {...props} className="bg-[#eeeaea] rounded-md m-[12px] p-[8px]  dark:bg-gray-800" />
+                          pre: ({ node, ...props }) => {
+                            const currentIndex = preIndex++;
+                            return(
+                              <div className='relative'>
+                              <pre id={`pretag-${currentIndex}`} {...props} className="bg-[#eeeaea] rounded-md m-[12px] p-[8px] dark:bg-gray-800" />
+                              <button onClick={() => handleCopy(currentIndex)} className='absolute top-2 right-10 flex text-sm items-center font-light '>
+                                {copyStatus[currentIndex] ? "Copied" : <CopyIcon />} {copyStatus[currentIndex] ? null : <p className="text-sm font-base">copy code</p>}
+                              </button>
                             </div>
-                          ),
+                            )
+                          },
                           ul: ({ node, ...props }) => (
                             <ul
                               {...props}
