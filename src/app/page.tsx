0af75d86ec7ac4b7fed242649cd3a301b5cf4af5
Pratham156 <pratham.chauhan@leapx.ai>
2024-07-17 06:47:04 +0530

    added the recursive fetch of url in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+import { compile } from "html-to-text";
+import { RecursiveUrlLoader } from "@langchain/community/document_loaders/web/recursive_url";
 
-export default function Home() {
+export default async function Home() {
+  const url = "https://js.langchain.com/v0.2/docs";
+
+  const compiledConvert = compile({ wordwrap: 130 }); // returns (text: string) => string;
+
+  const loader = new RecursiveUrlLoader(url, {
+    extractor: compiledConvert,
+    maxDepth: 200,
+    
+  });
+
+  const docs = await loader.load();
+  console.log(docs)
   return (
     <div>
-      
+
     </div>
   );
 }
