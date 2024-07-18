7cac8daaafba8599673b207374dc89593671aef7
Pratham271 <pratham19csu221@ncuindia.edu>
2024-07-18 06:38:25 +0530

    updated the icon

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ModeToggle } from './ui/toggle-mode'
+import { LogoIcon } from './ui/Icons'
 
 const Appbar = async() => {
 
@@ -7,8 +8,11 @@ const Appbar = async() => {
   return (
     <div className='flex justify-between items-center p-3 w-full top-0 bg-transparent z-50 border-b shadow-md'>
         <div className='flex flex-row items-center gap-5 md:px-10'>
-        <div className='flex font-bold text-lg'>
-          <h1 >Llama-Lounge</h1>
+        <div className='flex  text-lg items-center'>
+          <div className='h-8 w-8 rounded-md bg-black flex items-center justify-center mr-1'>
+            <LogoIcon/>
+          </div>
+          <h1 className='font-medium'>Llama-Lounge</h1>
         </div>
         </div>
         <div className='flex items-center bg-transparent md:px-24'>
