bd9e98053e6d96327c4c85e2e3542b9bae72a5e6
Pratham271 <pratham19csu221@ncuindia.edu>
2024-07-18 06:58:39 +0530

    testing CI

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -6,9 +6,9 @@ const Appbar = async() => {
 
   
   return (
-    <div className='flex justify-between items-center p-3 w-full top-0 bg-transparent z-50 border-b shadow-md'>
-        <div className='flex flex-row items-center gap-5 md:px-10'>
-        <div className='flex  text-lg items-center'>
+    <div className="flex justify-between items-center p-3 w-full top-0 bg-transparent z-50 border-b shadow-md">
+        <div className="flex flex-row items-center gap-5 md:px-10">
+        <div className="flex  text-lg items-center">
           <div className='h-8 w-8 rounded-md bg-black flex items-center justify-center mr-1'>
             <LogoIcon/>
           </div>
