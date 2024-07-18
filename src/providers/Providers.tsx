c5fc3b2caa3cd8521b54471c07fd4531650dd781
Pratham271 <pratham19csu221@ncuindia.edu>
2024-07-18 15:08:02 +0530

    added the next auth support

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,18 +3,21 @@
 import { ThemeProvider } from '@/components/theme-provider'
 import { RecoilRoot } from 'recoil'
 
+import { SessionProvider } from "next-auth/react" 
 
 export function Providers({children}: {children:React.ReactNode}){
     return(
-    <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-    >
-        <RecoilRoot>
-            {children}
-        </RecoilRoot>
-    </ThemeProvider>
+    <SessionProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <RecoilRoot>
+                {children}
+            </RecoilRoot>
+        </ThemeProvider>
+    </SessionProvider>
     )
 }
