38f41d0e4fe0320f91bfb4c0ec10bf8a7e0f3e78
Pratham271 <pratham19csu221@ncuindia.edu>
2024-07-18 07:05:53 +0530

    testing CI

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -9,13 +9,13 @@ const Appbar = async() => {
     <div className="flex justify-between items-center p-3 w-full top-0 bg-transparent z-50 border-b shadow-md">
         <div className="flex flex-row items-center gap-5 md:px-10">
         <div className="flex  text-lg items-center">
-          <div className='h-8 w-8 rounded-md bg-black flex items-center justify-center mr-1'>
+          <div className="h-8 w-8 rounded-md bg-black flex items-center justify-center mr-1">
             <LogoIcon/>
           </div>
-          <h1 className='font-medium'>Llama-Lounge</h1>
+          <h1 className="font-medium">Llama-Lounge</h1>
         </div>
         </div>
-        <div className='flex items-center bg-transparent md:px-24'>
+        <div className="flex items-center bg-transparent md:px-24">
             <ModeToggle/>
         </div>
     </div>
